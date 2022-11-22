680c6b90566fcad977685a873151b487d26d3e77
Mark Parvez <parveza3@ug150.eecg.toronto.edu>
2022-11-22 09:40:27 -0500

    huhhh

--- lab7/part2/part2.v
+++ lab7/part2/part2.v
@@ -23,26 +23,32 @@ module part2(iResetn,iPlotBox,iBlack,iColour,iLoadX,iXY_Coord,iClock,oX,oY,oColo
 
    //
    // Your code goes here
-   wire ld_x, ld_y, increment_count, clearing_screen, ld_colour, clear_count;
+   wire ld_x_init, ld_y_init, increment_count, clearing_screen, ld_colour, clear_count, ld_x_init_black, ld_y_init_black, ld_colour_black;
    wire [3:0] count;
    control #(.X_SCREEN_PIXELS(X_SCREEN_PIXELS), .Y_SCREEN_PIXELS(Y_SCREEN_PIXELS)) c0(.Resetn(iResetn), .PlotBox(iPlotBox), .Black(iBlack), 
               .Clock(iClock), .LoadX(iLoadX), .count(count),
-              .Done(oDone), .ld_x(ld_x), .ld_y(ld_y),
+              .Done(oDone), .ld_x_init(ld_x_init), .ld_y_init(ld_y_init),
               .increment_count(increment_count),
               .clearing_screen(clearing_screen),
               .Plot(oPlot),
               .ld_colour(ld_colour),
-              .clear_count(clear_count)
+              .clear_count(clear_count),
+              .ld_x_init_black(ld_x_init_black),
+              .ld_x_init_black(ld_y_init_black),
+              .ld_colour_black(ld_colour_black)
               );
 
    datapath #(.X_SCREEN_PIXELS(X_SCREEN_PIXELS), .Y_SCREEN_PIXELS(Y_SCREEN_PIXELS)) d0(.Resetn(iResetn), .Clock(iClock),
                .Colour(iColour), .XY_Coord(iXY_Coord), 
-               .ld_x(ld_x), .ld_y(ld_y), .increment_count(increment_count),
+               .ld_x_init(ld_x_init), .ld_y_init(ld_y_init), .increment_count(increment_count),
                .clearing_screen(clearing_screen), .oX(oX), .oY(oY),
                .oColour(oColour),
                .count(count),
                .ld_colour(ld_colour),
-               .clear_count(clear_count)
+               .clear_count(clear_count),
+              .ld_x_init_black(ld_x_init_black),
+              .ld_x_init_black(ld_y_init_black),
+              .ld_colour_black(ld_colour_black)
                );
    //
 
@@ -51,7 +57,8 @@ endmodule // part2
 module control(input Resetn, PlotBox, Black, Clock, LoadX, 
                input [3:0] count,
                output reg Done = 0,
-               output reg ld_x = 0, ld_y = 0, ld_colour = 0, increment_count = 0, 
+               output reg ld_x_init = 0, ld_y_init = 0, ld_colour = 0, increment_count = 0, 
+               output reg ld_x_init_black = 0, ld_y_init_black = 0, ld_colour_black = 0,
                output reg Plot = 0, clearing_screen = 0, clear_count = 0
                );
 
@@ -62,8 +69,9 @@ module control(input Resetn, PlotBox, Black, Clock, LoadX,
               S_LOAD_Y = 2,
               S_LOAD_Y_WAIT = 3,
               S_DRAW = 4,
-              S_DRAW_BLACK = 5,
-              S_DONE = 6;
+              S_LOAD_BLACK_WAIT = 5,
+              S_DRAW_BLACK = 6,
+              S_DONE = 7;
 
    parameter X_SCREEN_PIXELS = 8'd160;
    parameter Y_SCREEN_PIXELS = 7'd120;
@@ -76,6 +84,7 @@ module control(input Resetn, PlotBox, Black, Clock, LoadX,
          S_LOAD_Y: next_state = PlotBox ? S_LOAD_Y_WAIT : S_LOAD_Y;
          S_LOAD_Y_WAIT: next_state = PlotBox ? S_LOAD_Y_WAIT : S_DRAW;
          S_DRAW: next_state = (count == 4'b1111) ? S_DONE : S_DRAW;
+         S_LOAD_BLACK_WAIT = Black ? S_LOAD_BLACK_WAIT : S_DRAW_BLACK;
          S_DRAW_BLACK: next_state = (count == X_SCREEN_PIXELS*Y_SCREEN_PIXELS - 1) ? S_DONE : S_DRAW_BLACK;
          S_DONE: next_state = S_LOAD_X;
       endcase
@@ -87,9 +96,12 @@ module control(input Resetn, PlotBox, Black, Clock, LoadX,
    if (!Resetn) Done = 0;
 
       clear_count = 0;
-      ld_x = 0;
-      ld_y = 0;
+      ld_x_init = 0;
+      ld_y_init = 0;
+      ld_x_init_black = 0;
+      ld_y_init_black = 0;
       ld_colour = 0;
+      ld_colour_black = 0;
       increment_count = 0;
       Plot = 0;
       
@@ -97,11 +109,11 @@ module control(input Resetn, PlotBox, Black, Clock, LoadX,
          S_LOAD_X:
             clear_count = 1;
          S_LOAD_X_WAIT:
-            ld_x = 1;
+            ld_x_init = 1;
          // S_LOAD_Y:
-         //    ld_y = 1;
+         //    ld_y_init = 1;
          S_LOAD_Y_WAIT:begin
-            ld_y = 1;
+            ld_y_init = 1;
             ld_colour = 1;
          end
          S_DRAW: begin
@@ -109,9 +121,14 @@ module control(input Resetn, PlotBox, Black, Clock, LoadX,
             increment_count = 1;
             Plot = 1;
          end
-         S_DRAW_BLACK: begin
-            if (Black) clear_count = 1;
+         S_LOAD_BLACK_WAIT: begin
             Done = 0;
+            clear_count = 1;
+            ld_colour_black = 1; 
+            ld_x_init_black = 1;
+            ld_y_init_black = 1;   
+         end
+         S_DRAW_BLACK: begin
             increment_count = 1;
             Plot = 1;
             clearing_screen = 1;
@@ -125,7 +142,7 @@ module control(input Resetn, PlotBox, Black, Clock, LoadX,
    always @(posedge Clock)
    begin: state_FFs
       if (!Resetn) current_state <= S_LOAD_X;
-      else if (Black) current_state <= S_DRAW_BLACK;  
+      else if (Black) current_state <= S_LOAD_BLACK_WAIT;  
       else current_state <= next_state;
    end
 
@@ -135,7 +152,8 @@ endmodule
 module datapath(input Resetn, Clock,
                 input [2:0] Colour,
                 input [6:0] XY_Coord, 
-                input ld_x, ld_y, increment_count, clearing_screen, ld_colour, clear_count,
+                input ld_x_init, ld_y_init, increment_count, clearing_screen, ld_colour,
+                input clear_count, ld_x_init_black, ld_y_init_black, ld_colour_black,
                 output reg [7:0] oX = 0, 
                 output reg [6:0] oY = 0,
                 output reg [2:0] oColour = 0,
@@ -174,13 +192,21 @@ module datapath(input Resetn, Clock,
          count <= 0;
       end
       else begin
-         if (ld_x) x_init <= XY_Coord;
-         if (ld_y) y_init <= XY_Coord;
-         if (ld_colour) oColour <= clearing_screen ? 0 : Colour;
+         if (ld_x_init) x_init <= XY_Coord;
+         if (ld_y_init) y_init <= XY_Coord;
+         if (ld_x_init_black) x_init <= 0;
+         if (ld_y_init_black) y_init <= 0;
+         if (ld_colour) oColour <= Colour;
+         if (ld_colour_black) oColour <= 0;
+
+         if (increment_count) begin 
+            count <= count + 1;
+            x_offset <= x_offset + 1;
+         end
 
          if (clearing_screen) begin
-            if (x_offset == X_SCREEN_PIXELS) begin
-               if(y_offset != Y_SCREEN_PIXELS) begin
+            if (x_offset == X_SCREEN_PIXELS-1) begin
+               if(y_offset != Y_SCREEN_PIXELS-1) begin
                   x_offset <= 0;
                   y_offset <= y_offset + 1;
                end
@@ -202,11 +228,7 @@ module datapath(input Resetn, Clock,
                end
             end
          end
-
-         if (increment_count) begin 
-            count <= count + 1;
-            x_offset <= x_offset + 1;
-         end
       end
    end
 endmodule
+
